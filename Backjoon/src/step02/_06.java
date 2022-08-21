ccc5a57d2dfca4d8d512cbad47821a001230f50b
leemh4390 <leemh44390@gmail.com>
2022-08-21 22:43:51 +0900

    백준 step2

--- Backjoon/src/step02/_06.java
+++ Backjoon/src/step02/_06.java
@@ -2,24 +2,24 @@ package step02;
 
 	import java.util.Scanner;
 	
-public class _05 {
+public class _06 {
 	public static void main(String[] args) {
-	
+		
 	Scanner sc = new Scanner(System.in);
 	
 	int H = sc.nextInt();
 	int M = sc.nextInt();
+	int C = sc.nextInt();
 	
-	if(M < 45) {
-		H --;
-	M =  60 - (45 - M );
-	if(H < 0 ) {
-		H = 23;
-	}
+	int A = H*60 + M+ C; 
 	
-	}
+	H = A / 60;
+	M = A % 60;
 	
-			
+	if (H >= 24) {
+		H = H - 24; }
+	System.out.println(H + " " + M);
 	sc.close();
+	
 	}
 }
