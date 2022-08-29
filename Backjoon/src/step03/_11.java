e76cee27f65c3e70e6d5eaa2fe27b94a4c4fc61a
leemh4390 <leemh44390@gmail.com>
2022-08-29 10:38:17 +0900

    2022/08/29

---
+++ Backjoon/src/step03/_11.java
@@ -0,0 +1,26 @@
+package step03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11 {
+	public static void main(String[] args) throws IOException{
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String str = br.readLine();
+	StringTokenizer st = new StringTokenizer(str," ");
+	
+	int arr[] = {1, 10, 4, 9, 2, 3, 8, 5, 7, 6};
+	
+	int a = Integer.parseInt(st.nextToken());
+	int b = Integer.parseInt(st.nextToken());
+	
+		for(int i = 0; i <= 1; i++) {
+			
+			
+			
+		}
+	}
+}
---
+++ Ch05/src/sub05/InheritTest.java
@@ -0,0 +1,84 @@
+package sub05;
+
+/*
+ * 날짜 : 2022/08/29 
+ * 이름 : 이민혁
+ * 내용 : Java 클래스 상속 실습하기
+ * 
+ * 상속(Inheritance)
+ * - 기존 클래스의 속성과 기능을 그대로 자식 클래스에게 물려주는 클래스 확장문법
+ * - 공통적인 로직 내용을 부모클래스에 두고 자식 클래스에서 상속 받아 일관성 있는 프로그래밍 수행
+ * - 부모 클래스의 속성 접근 권한은 protected 선언해서 지식 클래스에서만 참조 되도록 수정
+ */
+
+class Parent {
+	private int num1;
+	private int num2;
+	
+	public Parent (int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int plus() {
+		return num1 + num2;
+	}
+}
+
+
+class Child extends Parent {
+	private int num3;
+	private int num4;
+
+	public Child (int num1, int num2, int num3,int num4) {
+		super(num1,num2);
+		this.num3 = num3;
+		this.num4 = num4;
+		
+	}
+	
+	public int minus() {
+		return num3-num4;
+	}
+	
+}
+
+public class InheritTest {
+	public static void main(String[] args) {
+		// 객체 생성
+		Parent p = new Parent(1,2);
+		int r1 = p.plus();
+		
+		System.out.println("r1 : " + r1);
+		
+		Child c = new Child(2,3,4,5);
+		int r2 = c.plus();
+		int r3 = c.minus();
+		
+		System.out.println("r2 : " + r2);
+		System.out.println("r3 : " + r3);
+		
+		
+		Sedan sonata = new Sedan("소나타", "흰색", 0,2000);
+		Truck bongo = new Truck("봉고", "파란색", 0,0);
+		
+		sonata.speedup(100);
+		sonata.speedTurbo(80);
+		sonata.show();
+		
+		bongo.load(100);
+		bongo.speedup(80);
+		bongo.show();
+		
+		
+		//account 자식클래스 생성
+		
+		StockAccount kb = new StockAccount("kb증권", "101-12-1001", "홍길동", 0,"삼성전자",0,0);
+		kb.deposit(1000000);
+		kb.buy(10, 70000);
+		kb.sell(5, 75000);
+		kb.withdraw(200000);
+		kb.show();
+		
+	}
+}
