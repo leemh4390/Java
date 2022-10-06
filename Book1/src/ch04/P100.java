5f0b4d92dfa476183d282fdd85f706cd4e9eaa16
leemh44390 <leemh44390@gmail.com>
2022-10-06 13:50:22 +0900

    2022/10/06

--- Book1/src/ch04/P100.java
+++ Book1/src/ch04/P100.java
@@ -1,5 +1,26 @@
 package ch04;
 
 public class P100 {
-
+		public static void main(String[] args) {
+			
+			int Score = 50;
+			char grade;
+			
+			if (Score >= 90) {
+				grade = 'A';
+			} else if (Score >= 80) {
+				grade = 'B';
+			} else if (Score >= 70) {
+				grade = 'C';
+			} else if (Score >= 60) {
+				grade = 'D';
+			} else {
+				grade = 'F';
+			}
+			
+			System.out.println("점수는 : " + Score + "입니다.");
+			System.out.println("등급은 : " + grade + "입니다.");
+		}
+		
+			
 }
---
+++ Book1/src/ch04/P100_1.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class P100_1 {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 9;
+		int max;
+		
+		if (a > b) {
+			max = a;
+		} else 
+			max = b;
+	}
+}
